a3f3c8e6d097d0073600a5e52a886fdd298efc28
rf <56006623+rf742@users.noreply.github.com>
2022-04-03 21:13:23 -0400

    cleaned up code, but still 2d bugs

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 import math
 
-K = 8.988E9 #Coulombs constant
+K = 8.988E9 # Coulomb constant
 e = -1.602217662E-19 # charge on electron
+
 class pointCharge():
-    def __init__(self, x,y,q):
+    def __init__(self, x, y, q):
         self.x = x
         self.y = y
         self.q = q
@@ -15,24 +16,28 @@ class pointCharge():
         self.netangle = 0
 
 def main():
-    bob = [pointCharge(0,2E-7,2*e), pointCharge(0,0,-3*e), pointCharge(4E-7,0,-5*e)]
-    for i, point in enumerate(bob):
-        for j, p in enumerate(bob):
+    charges = [pointCharge(0,4,-6E-6), pointCharge(0,0,4E-6), pointCharge(3,0,2E-6)]
+#   see https://farside.ph.utexas.edu/teaching/316/lectures/node20.html
+
+    for i, p1 in enumerate(charges):
+        for j, p2 in enumerate(charges):
             if i != j:
-                x_dist = (p.x - point.x)
-                y_dist = (p.y - point.y)
-                dist = ((x_dist**2)+(y_dist**2))**.5
-                totalforce = (K*point.q * p.q)/(dist**2)
+                x_dist = (p2.x - p1.x)
+                y_dist = (p2.y - p1.y)
+                dist = math.sqrt((x_dist**2)+(y_dist**2))
+                totalforce = (K*p1.q * p2.q)/(dist**2)
                 angle = math.atan2(y_dist,x_dist)
-                bob[i].fx += totalforce*math.cos(angle)
-                bob[i].fy += totalforce*math.sin(angle)
-    for p in bob:
+                fx = totalforce*math.cos(angle)
+                fy = totalforce*math.sin(angle)
+                p1.fx += fx
+                p2.fy += fy
+
+    for p in charges:
         p.magf = math.sqrt(p.fx**2 + p.fy**2)
         p.netangle = math.atan2(p.fy,p.fx)
-        print(f'The {p.q} C charge @ {p.x},{p.y}')
-        print(f'experiences a force of total magnitude {abs(p.magf)} N\n')
-        print(f'This force occurs at an angle of {p.netangle} radians')
-        print(f'                                 {p.netangle*180/math.pi} degrees')
-
+        print(f'For the: {p.q:.2E} C charge @ {p.x},{p.y}:')
+        print(f' |Fnet| = {(p.magf):.2E} N')
+        print(f'  Fnet  = {p.fx:.2E}i + {p.fy:.2E}j')
+        print(f'  Angle = {p.netangle:.2F} rads ({p.netangle*180/math.pi:.2F} deg)\n')
 if __name__ == "__main__":
     main()
