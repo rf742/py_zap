397ac9d7233cc8c363fd5031d80b99c07f8bf848
rf <56006623+rf742@users.noreply.github.com>
2022-04-09 23:18:15 -0400

    refactored tables.py

--- tables.py
+++ tables.py
@@ -9,21 +9,23 @@ tosci = lambda num: f'{num:.2e}'
 
 
 def printTable(points, angleUnit):
-    console=Console()
+    console=Cfonsole()
     table = Table(show_header=True, header_style="bold magenta",)
     headers = ["x","y","q","Net Force", "F_x", "F_y", "Angle"]
     for i in headers:
         table.add_column(str(i), justify='center')
     for p in points:
-        table.add_row(f'{p.x:.2}',f'{p.y:.2}',tosci(p.q),tosci(p.magf),tosci(p.fx),tosci(p.fy),f'{p.resolve(angleUnit)[1]:.2f}')
+        mag, ang = p.resolve(angleUnit)
+        table.add_row(f'{p.x:.2}',f'{p.y:.2}',tosci(p.q),tosci(mag),tosci(p.fx),tosci(p.fy),f'{ang:.2f}')
     console.print(table)
 
 def uglyprint(points):
     for p in points:
+        mag, ang = p.resolve()
         print(f'For the: {p.q:.2E} C charge @ {p.x},{p.y}:')
-        print(f' |Fnet| = {p.resolve()[0]:.2E} N')
+        print(f' |Fnet| = {mag:.2E} N')
         print(f'  Fnet  = {p.fx:.2E}i + {p.fy:.2E}j')
-        print(f'  Angle = {p.resolve()[1]:.2F} rads ({p.resolve(True)[1]:.2F} deg)')
+        print(f'  Angle = {ang:.2F} rads ({p.resolve(True)[1]:.2F} deg)')
         print('\n')
 
 def pTable(points, deg):
