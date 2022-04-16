3769e103b949fe0738464c10347b9d1cddf176b6
rf <56006623+rf742@users.noreply.github.com>
2022-04-16 10:13:52 -0400

    linted main.py with flake8

--- main.py
+++ main.py
@@ -2,27 +2,44 @@
 import sys
 import math
 import argparse
-import csv
 import time
 
 import tables
-import point
 import filetools
 
-K = 8.988E9 # Coulomb constant
-e = -1.602217662E-19 # charge on electron
-G = 6.674E-11 # Gravitational constant
+K = 8.988E9           # Coulomb constant
+e = -1.602217662E-19  # charge on electron
+G = 6.674E-11         # Gravitational constant
 
-        
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--gravity', action='store_true', help='The program will compute gravitational force instead of electrical force. [Put mass where charge normally goes in input file]')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-i', '--input-data', action='store', dest='infile', required='true', help='File containingdata on your point charges [x,y,q] ')
-    parser.add_argument('-c', '--csv', action='store_true', help='output data to csv')
-    parser.add_argument('-d', '--degrees', action='store_true', help='display angle in degrees instead of radians.')
+    parser.add_argument('-g',
+                        '--gravity',
+                        action='store_true',
+                        help='The program will compute gravitational '
+                        'force instead of electrical force. [Put mass '
+                        'where charge normally goes in input file]')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true')
+    parser.add_argument('-i',
+                        '--input-data',
+                        action='store',
+                        dest='infile',
+                        required='true',
+                        help='File containing data on your point '
+                        'charges [x,y,q]')
+    parser.add_argument('-c',
+                        '--csv',
+                        action='store_true',
+                        help='output data to csv')
+    parser.add_argument('-d',
+                        '--degrees',
+                        action='store_true',
+                        help='display angle in degrees instead '
+                        'of radians.')
     return parser.parse_args()
-
 
 
 def validate_masses(points):
@@ -32,10 +49,11 @@ def validate_masses(points):
             print("Exiting")
             sys.exit(1)
 
+
 def main():
     args = get_args()
     verbose = args.verbose
-    csvoutput=args.csv
+    csvoutput = args.csv
     deg = args.degrees
     FORMULACONSTANT = K if not args.gravity else G
     charges = filetools.getCharges(args.infile)
@@ -50,32 +68,33 @@ def main():
                 try:
                     totalforce = (FORMULACONSTANT*p1.q * p2.q)/(dist**2)
                 except ZeroDivisionError:
-                    print("Multiple point charges cannot be located at same coordinate.")
+                    print("Multiple point charges cannot be located at "
+                          "same coordinate.")
                     sys.exit(1)
-                angle = math.atan2(y_dist,x_dist)
+                angle = math.atan2(y_dist, x_dist)
                 fx = abs(totalforce*math.cos(angle))
                 fy = abs(totalforce*math.sin(angle))
                 if not args.gravity:
                     if p2.x < p1.x and (p1.q * p2.q < 0):
-                        fx=-fx
+                        fx = -fx
                     elif p2.x > p1.x and (p1.q * p2.q > 0):
-                        fx=-fx
+                        fx = -fx
                     if p2.y < p1.y and (p1.q * p2.q < 0):
-                        fy=-fy
+                        fy = -fy
                     elif p2.y > p1.y and (p1.q * p2.q > 0):
-                        fy=-fy
+                        fy = -fy
                 elif args.gravity:
                     if p2.x < p1.x:
-                        fx=-fx
+                        fx = -fx
                     if p2.y < p1.y:
-                        fy=-fy
+                        fy = -fy
                 else:
                     print("Error, exiting")
                     sys.exit(1)
                 p1.fx += fx
                 p1.fy += fy
                 if verbose:
-                    print(f'--- Verbose Output ---')
+                    print('--- Verbose Output ---')
                     print(f'force of particle {j} on particle {i}')
                     print(f'fx = {fx:.2E}')
                     print(f'fy = {fy:.2E}')
@@ -83,15 +102,17 @@ def main():
                     print(f'x: {p2.x}-{p1.x} = {p2.x-p1.x}')
                     print(f'Fnet = {math.hypot(fx,fy):.2E}')
                     print(f'angle = {angle*180/math.pi:.2F}')
-                    print(f'----------------------\n')
+                    print('----------------------\n')
     for p in charges:
         p.magf = math.sqrt(p.fx**2 + p.fy**2)
-        p.netangle = math.atan2(p.fy,p.fx)
+        p.netangle = math.atan2(p.fy, p.fx)
     if csvoutput:
         csvfilename = 'zap_data_' + time.strftime('%Y%m%d%H%M%S') + '.csv'
         filetools.csvout(csvfilename, charges)
         if verbose:
             print("writing csv data to: " + csvfilename)
-    tables.pTable(charges,deg)
+    tables.pTable(charges, deg)
+
+
 if __name__ == "__main__":
     main()
