9bf481e9b0ea4a43305aa244271fb1d884a3f973
rf <56006623+rf742@users.noreply.github.com>
2022-04-16 10:33:52 -0400

    linted tables.py with flake8

--- tables.py
+++ tables.py
@@ -1,23 +1,28 @@
-#!/usr/bin/env python3
-
 from rich.console import Console
 from rich.table import Table
-import math
 
 
-tosci = lambda num: f'{num:.2e}'
+def tosci(num):
+    return f'{num:.2e}'
 
 
 def printTable(points, angleUnit):
-    console=Console()
+    console = Console()
     table = Table(show_header=True, header_style="bold magenta",)
-    headers = ["x","y","q","Net Force", "F_x", "F_y", "Angle"]
+    headers = ["x", "y", "q", "Net Force", "F_x", "F_y", "Angle"]
     for i in headers:
         table.add_column(str(i), justify='center')
     for p in points:
         mag, ang = p.resolve(angleUnit)
-        table.add_row(f'{p.x:.2}',f'{p.y:.2}',tosci(p.q),tosci(mag),tosci(p.fx),tosci(p.fy),f'{ang:.2f}')
+        table.add_row(f'{p.x:.2}',
+                      f'{p.y:.2}',
+                      tosci(p.q),
+                      tosci(mag),
+                      tosci(p.fx),
+                      tosci(p.fy),
+                      f'{ang:.2f}')
     console.print(table)
+
 
 def uglyprint(points):
     for p in points:
@@ -28,10 +33,10 @@ def uglyprint(points):
         print(f'  Angle = {ang:.2F} rads ({p.resolve(True)[1]:.2F} deg)')
         print('\n')
 
+
 def pTable(points, deg):
     try:
-        printTable(points,deg)
-    except:
+        printTable(points, deg)
+    except ImportError:
         print("Error: Defaulting to plain text output")
         uglyprint(points)
-
